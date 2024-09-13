324c8424dd94bfa38d46f880926c3c46c98330a6
ken-william <70495780+ken-william@users.noreply.github.com>
2024-09-13 11:32:14 +0200

    Update routes.py
    Update routes py 5

--- service/routes.py
+++ service/routes.py
@@ -140,7 +140,7 @@ def check_content_type(media_type):
     """Checks that the media type is correct"""
     content_type = request.headers.get("Content-Type")
     if content_type and content_type == media_type:
-        return
+    return
     app.logger.error("Invalid Content-Type: %s", content_type)
     abort(
         status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
